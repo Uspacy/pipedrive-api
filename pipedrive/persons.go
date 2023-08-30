aa1992e17a979c7bbe11b905721d3eb7eeaac358
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-08-30 13:00:02 +0300

    Update persons.go

--- pipedrive/persons.go
+++ pipedrive/persons.go
@@ -12,7 +12,7 @@ import (
 // Pipedrive API docs: https://developers.pipedrive.com/docs/api/v1/#!/Persons
 type PersonsService service
 
-type Email struct {
+type PhoneEmail struct {
 	Label   string `json:"label"`
 	Value   string `json:"value"`
 	Primary bool   `json:"primary"`
@@ -31,34 +31,31 @@ type Person struct {
 		ActiveFlag bool   `json:"active_flag"`
 		Value      int    `json:"value"`
 	} `json:"owner_id"`
-	OrgID                       interface{} `json:"org_id"`
-	Name                        string      `json:"name"`
-	FirstName                   string      `json:"first_name"`
-	LastName                    string      `json:"last_name"`
-	OpenDealsCount              int         `json:"open_deals_count"`
-	RelatedOpenDealsCount       int         `json:"related_open_deals_count"`
-	ClosedDealsCount            int         `json:"closed_deals_count"`
-	RelatedClosedDealsCount     int         `json:"related_closed_deals_count"`
-	ParticipantOpenDealsCount   int         `json:"participant_open_deals_count"`
-	ParticipantClosedDealsCount int         `json:"participant_closed_deals_count"`
-	EmailMessagesCount          int         `json:"email_messages_count"`
-	ActivitiesCount             int         `json:"activities_count"`
-	DoneActivitiesCount         int         `json:"done_activities_count"`
-	UndoneActivitiesCount       int         `json:"undone_activities_count"`
-	ReferenceActivitiesCount    int         `json:"reference_activities_count"`
-	FilesCount                  int         `json:"files_count"`
-	NotesCount                  int         `json:"notes_count"`
-	FollowersCount              int         `json:"followers_count"`
-	WonDealsCount               int         `json:"won_deals_count"`
-	RelatedWonDealsCount        int         `json:"related_won_deals_count"`
-	LostDealsCount              int         `json:"lost_deals_count"`
-	RelatedLostDealsCount       int         `json:"related_lost_deals_count"`
-	ActiveFlag                  bool        `json:"active_flag"`
-	Phone                       []struct {
-		Value   string `json:"value"`
-		Primary bool   `json:"primary"`
-	} `json:"phone"`
-	Email                           []Email     `json:"email"`
+	OrgID        	               	interface{} `json:"org_id"`
+	Name            	            string      `json:"name"`
+	FirstName           	        string      `json:"first_name"`
+	LastName                	    string      `json:"last_name"`
+	OpenDealsCount              	int         `json:"open_deals_count"`
+	RelatedOpenDealsCount	       	int         `json:"related_open_deals_count"`
+	ClosedDealsCount        	    int         `json:"closed_deals_count"`
+	RelatedClosedDealsCount     	int         `json:"related_closed_deals_count"`
+	ParticipantOpenDealsCount  		int         `json:"participant_open_deals_count"`
+	ParticipantClosedDealsCount 	int         `json:"participant_closed_deals_count"`
+	EmailMessagesCount         		int         `json:"email_messages_count"`
+	ActivitiesCount             	int         `json:"activities_count"`
+	DoneActivitiesCount         	int         `json:"done_activities_count"`
+	UndoneActivitiesCount       	int         `json:"undone_activities_count"`
+	ReferenceActivitiesCount	    int         `json:"reference_activities_count"`
+	FilesCount          	        int         `json:"files_count"`
+	NotesCount        	          	int         `json:"notes_count"`
+	FollowersCount              	int         `json:"followers_count"`
+	WonDealsCount           	    int         `json:"won_deals_count"`
+	RelatedWonDealsCount	        int         `json:"related_won_deals_count"`
+	LostDealsCount              	int         `json:"lost_deals_count"`
+	RelatedLostDealsCount   	    int         `json:"related_lost_deals_count"`
+	ActiveFlag                  	bool        `json:"active_flag"`
+	Phone                       	[]PhoneEmail `json:"phone"`
+	Email                           []PhoneEmail     `json:"email"`
 	FirstChar                       string      `json:"first_char"`
 	UpdateTime                      string      `json:"update_time"`
 	AddTime                         string      `json:"add_time"`
